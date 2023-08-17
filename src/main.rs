34290194202ec27d435fe62e24d6a735b7bd0958
iamBharatManral <justbharatmanral@gmail.com>
2023-08-17 07:30:56 +0530

    feat: add player with moving upward capability when pressed SPACE

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,108 @@
 use bracket_lib::prelude::*;
 
 struct State {
-    mode: GameMode
+    mode: GameMode,
+    player: Player,
+    frame_time: f32,
 }
 
 enum GameMode {
     Menu,
     Playing,
-    End
+    End,
 }
 
-impl GameState for State{
-    fn tick(&mut self, ctx: &mut BTerm){
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'))
+    }
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0
+        }
+    }
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
         match self.mode {
             GameMode::Menu => self.main_menu(ctx),
             GameMode::Playing => self.play(ctx),
-            GameMode::End => self.end(ctx)
+            GameMode::End => self.end(ctx),
         }
     }
 }
 
 impl State {
     fn new() -> Self {
-        State{
-            mode: GameMode::Menu
+        State {
+            mode: GameMode::Menu,
+            player: Player::new(5, 25),
+            frame_time: 0.0,
         }
     }
     fn play(&mut self, ctx: &mut BTerm) {
-        // TODO: fill in stub later
-        self.mode = GameMode::End
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End
+        }
     }
-    fn restart(&mut self){
+    fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing
     }
-    fn main_menu(&mut self, ctx: &mut BTerm){
+    fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "🪽 Welcome to Flappy Game 🪽!");
         ctx.print_centered(8, "(P) Play Game 🪽");
         ctx.print_centered(9, "(Q) Quit Game ❎");
-        if let Some(key) = ctx.key{
+        if let Some(key) = ctx.key {
             match key {
                 VirtualKeyCode::P => self.restart(),
                 VirtualKeyCode::Q => ctx.quitting = true,
-                _ => ()
+                _ => (),
             }
         }
     }
-    fn end(&mut self, ctx: &mut BTerm){
+    fn end(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead 💀!");
         ctx.print_centered(8, "(P) Play Again 🪽");
@@ -55,7 +111,7 @@ impl State {
             match key {
                 VirtualKeyCode::P => self.restart(),
                 VirtualKeyCode::Q => ctx.quitting = true,
-                _ => ()
+                _ => (),
             }
         }
     }
